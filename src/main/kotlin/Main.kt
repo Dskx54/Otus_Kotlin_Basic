596dd7485c4c0716dfa44fa6eac0bbf0dfc4d2a2
starcz40 <starcz40@yandex-team.ru>
2022-10-13 16:50:03 +0300

    ex1_1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    println("Hello World!_1")
+    println("Hello World!_1_1")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
