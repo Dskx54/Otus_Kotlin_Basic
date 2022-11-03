8cf9b1e9858632c72e593ea4953ae694a497a373
starcz40 <starcz40@yandex-team.ru>
2022-11-03 23:48:22 +0300

    HW-3 trrd try

--- src/main/kotlin/hw3/Task-2.kt
+++ src/main/kotlin/hw3/Task-2.kt
@@ -1,7 +1,6 @@
 
 package hw3
 
-import java.lang.StringBuilder
 import java.util.*
 //enum class RomanNumerals{
 //    I,
@@ -15,35 +14,42 @@ fun main(){
 
     println("Input number")
     val input = Scanner(System.`in`)
-    val number = input.next()
-    val str = StringBuilder()
-//    val firstDigit = when (number[0]){
-//        0 -> ""
-//        1 -> "I"
-//        2 -> "II"
-//        3 -> "III"
-//        4 -> "IV"
-//        5 -> "V"
-//        6 -> "VI"
-//        7 -> "VII"
-//        8 -> "VIII"
-//        9 -> "IX"
-//        else -> {}
-//    }
-//    val secondDigit = when (number[1]){
-//        1 -> "X"
-//        2 -> "XX"
-//        3 -> "XXX"
-//        4 -> "XL"
-//        5 -> "L"
-//        6 -> "LX"
-//        7 -> "LXX"
-//        8 -> "LXXX"
-//        9 -> "XC"
-//        else -> {}
-//    }
-//    println(firstDigit + secondDigit)
-
+    val number = input.nextInt()
+    if (number < 100) {
+        val firstDigit = when (number / 10) {
+            1 -> "X"
+            2 -> "XX"
+            3 -> "XXX"
+            4 -> "XL"
+            5 -> "L"
+            6 -> "LX"
+            7 -> "LXX"
+            8 -> "LXXX"
+            9 -> "XC"
+            else -> {}
+        }
+        val secondDigit = when (number % 10) {
+            0 -> ""
+            1 -> "I"
+            2 -> "II"
+            3 -> "III"
+            4 -> "IV"
+            5 -> "V"
+            6 -> "VI"
+            7 -> "VII"
+            8 -> "VIII"
+            9 -> "IX"
+            else -> {}
+        }
+        if (number < 10) {
+            println("$secondDigit")
+        } else {
+            println("$firstDigit$secondDigit")
+        }
+    }
+    else {
+        println("Your number $number is more than 100")
+    }
 
 
 
